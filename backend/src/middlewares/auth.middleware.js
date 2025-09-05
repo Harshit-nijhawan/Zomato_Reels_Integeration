628b4b4465498153c2e4347d4619a8328bf76156
Harshit <nijhawanharshit58@gmail.com>
2025-09-05 17:04:53 +0530

    first commit

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,71 @@
+const foodPartnerModel = require("../models/foodpartner.model")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken");
+
+
+async function authFoodPartnerMiddleware(req, res, next) {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Please login first"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const foodPartner = await foodPartnerModel.findById(decoded.id);
+
+        req.foodPartner = foodPartner
+
+        if (!req.foodPartner) {
+            return res.status(401).json({ message: "Unauthorized: Food partner not found" });
+        }
+
+        next()
+
+    } catch (err) {
+
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+
+    }
+
+}
+
+async function authUserMiddleware(req, res, next) {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Please login first"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.id);
+
+        req.user = user
+
+        next()
+
+    } catch (err) {
+
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+
+    }
+
+}
+
+module.exports = {
+    authFoodPartnerMiddleware,
+    authUserMiddleware
+}
