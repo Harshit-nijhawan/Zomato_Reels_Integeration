acc8bff4b9315ae72432d185f9f1127e815adbd1
Harshit <nijhawanharshit58@gmail.com>
2025-09-06 13:52:27 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
